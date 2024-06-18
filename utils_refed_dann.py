52b0a79e3eecda97009fb3c0ac248fb3da82d478
MaloDesbois <malodesbois@gmail.com>
2024-06-18 10:40:09 +0200

    Create utils_refed_dann.py

---
+++ utils_refed_dann.py
@@ -0,0 +1,102 @@
+class ReverseLayerF(Function):
+
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = grad_output.neg() * ctx.alpha
+
+        return output, None
+
+  
+class SupervisedContrastiveLoss(nn.Module):
+    def __init__(self, temperature=0.07, min_tau=.07, max_tau=1., t_period=50, eps=1e-7):
+    #def __init__(self, temperature=1., min_tau=.07, max_tau=1., t_period=50, eps=1e-7):
+        """
+        Implementation of the loss described in the paper Supervised Contrastive Learning :
+        https://arxiv.org/abs/2004.11362
+
+        :param temperature: int
+        """
+        super(SupervisedContrastiveLoss, self).__init__()
+        self.temperature = temperature
+        self.min_tau = min_tau
+        self.max_tau = max_tau
+        self.t_period = t_period
+        self.eps = eps
+
+    def forward(self, projections, targets, epoch=1):
+        """
+        :param projections: torch.Tensor, shape [batch_size, projection_dim]
+        :param targets: torch.Tensor, shape [batch_size]
+        :return: torch.Tensor, scalar
+        """
+        device = torch.device("cuda") if projections.is_cuda else torch.device("cpu")
+
+
+        dot_product = torch.mm(projections, projections.T)
+
+        dot_product_tempered = dot_product / self.temperature
+
+        # Minus max for numerical stability with exponential. Same done in cross entropy. Epsilon added to avoid log(0)
+        stab_max, _ = torch.max(dot_product_tempered, dim=1, keepdim=True)
+        exp_dot_tempered = (
+            torch.exp(dot_product_tempered - stab_max.detach() ) + 1e-5
+        )
+
+        mask_similar_class = (targets.unsqueeze(1).repeat(1, targets.shape[0]) == targets).to(device)
+        mask_anchor_out = (1 - torch.eye(exp_dot_tempered.shape[0])).to(device)
+        mask_combined = mask_similar_class * mask_anchor_out
+        cardinality_per_samples = torch.sum(mask_combined, dim=1)
+
+        log_prob = -torch.log(exp_dot_tempered / (torch.sum(exp_dot_tempered * mask_anchor_out, dim=1, keepdim=True)))
+        #### FILTER OUT POSSIBLE NaN PROBLEMS ####
+        mdf = cardinality_per_samples!=0
+        cardinality_per_samples = cardinality_per_samples[mdf]
+        log_prob = log_prob[mdf]
+        mask_combined = mask_combined[mdf]
+        #### #### #### #### #### #### #### #### ####
+
+        supervised_contrastive_loss_per_sample = torch.sum(log_prob * mask_combined, dim=1) / cardinality_per_samples
+        supervised_contrastive_loss = torch.mean(supervised_contrastive_loss_per_sample)
+        return supervised_contrastive_loss
+
+def sim_dist_specifc_loss_spc(spec_emb, ohe_label, ohe_dom, scl, epoch):
+    norm_spec_emb = nn.functional.normalize(spec_emb)
+    hash_label = {}
+    new_combined_label = []
+    for v1, v2 in zip(ohe_label, ohe_dom):
+        key = "%d_%d"%(v1,v2)
+        if key not in hash_label:
+            hash_label[key] = len(hash_label)
+        new_combined_label.append( hash_label[key] )
+    new_combined_label = torch.tensor(np.array(new_combined_label), dtype=torch.int64)
+    return scl(norm_spec_emb, new_combined_label, epoch=epoch)
+def sup_contra_Cplus2_classes(emb, ohe_label, ohe_dom, scl, epoch):
+    norm_emb = nn.functional.normalize(emb)
+    C = ohe_label.max() + 1
+    new_combined_label = [v1 if v2==8 else C+v2 for v1, v2 in zip(ohe_label, ohe_dom)]
+    new_combined_label = torch.tensor(np.array(new_combined_label), dtype=torch.int64)
+    return scl(norm_emb, new_combined_label, epoch=epoch)
+
+
+def evaluation(model, dataloader, device):
+    model.eval()
+    tot_pred = []
+    tot_labels = []
+    for xm_batch, y_batch in dataloader:
+        x_batch,mask_batch = xm_batch[:,:,:2],xm_batch[:,:,2]
+        x_batch = x_batch.to(device)
+        y_batch = y_batch.to(device)
+        mask_batch = mask_batch.to(device)
+        pred = model(x_batch,mask_batch)[0]
+        pred_npy = np.argmax(pred.cpu().detach().numpy(), axis=1)
+        tot_pred.append( pred_npy )
+        tot_labels.append( y_batch.cpu().detach().numpy())
+    tot_pred = np.concatenate(tot_pred)
+    tot_labels = np.concatenate(tot_labels)
+    return tot_pred, tot_labels
